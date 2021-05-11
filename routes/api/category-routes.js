217d5589a2a73f20cb3adf8fdd0d248c255b8bcc
bdcii <darrell.chess@gmail.com>
2021-05-10 20:00:11 -0400

    added get routes for category

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -3,9 +3,16 @@ const { Category, Product } = require('../../models');
 
 // The `/api/categories` endpoint
 
-router.get('/', (req, res) => {
-  // find all categories
-  // be sure to include its associated Products
+router.get('/', async (req, res) => {
+  try {
+    const categoryData = await Category.findAll({
+      include: [Product]
+    });
+    res.status(200).json(categoryData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+
 });
 
 router.get('/:id', (req, res) => {
@@ -17,7 +24,7 @@ router.post('/', (req, res) => {
   // create a new category
 });
 
-router.put('/:id', (req, res) => {
+router.put('/:id',  (req, res) => {
   // update a category by its `id` value
 });
 
