256f9c747c37ae681219f22ed994768b5be39efe
bdcii <darrell.chess@gmail.com>
2021-05-10 21:27:06 -0400

    added route to get tag by id

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -17,7 +17,20 @@ router.get('/', async (req, res) => {
 
 });
 
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
+  try {
+    const tagData = await Tag.findByPk(req.params.id, {
+
+      include: [
+        {
+          model: Product, through: ProductTag
+        },
+      ]
+    });
+    res.status(200).json(tagData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
   // find a single tag by its `id`
   // be sure to include its associated Product data
 });
